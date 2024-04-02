adcb2aa7b3034cd451eeb7fe2f4fa9ad7a305908
Pietra Renner <pietra.renner@gmail.com>
2024-04-02 19:10:20 -0300

    changes

--- jantar_canibais.c
+++ jantar_canibais.c
@@ -2,62 +2,111 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 
-int travessa_vazia = 0;
-int porcoes = 0;
-int max_porcoes = 0;
+// int travessa_vazia = 0;
+// int porcoes = 0;
+// int max_porcoes = 0;
 
-pthread_mutex_t mtx_travessa_vazia, mtx_porcoes;
+pthread_mutex_t protege_porcoes;
+sem_t cozinhando, enchendo, comida;
+// pq tem que ser volatile?
+volatile int porcoes = 0; 
+
+void *chama_cozinheiro(void *arg)
+{
+    int id = *((int *) arg);
+    printf("o canibal %d chamou o cozinheiro\n", id);
+    // sleep(1);
+}
+
+void *come(void *arg)
+{
+    int id = *((int *) arg);
+    printf("o canibal %d está comendo\n", id);
+    sleep(2);
+}
+
+void *enche_travessa()
+{
+    printf("o cozinheiro está enchendo a travessa\n");
+    // printf("a quantidade total de porções é %d\n", porcoes);
+    sleep(2);
+}
 
 void *canibal(void *arg) 
 {
     int id = *((int *) arg);
+    // int id = id;
     while(1)
     {
-        pthread_mutex_lock(&mtx_porcoes);
-        if(porcoes <= 0) 
+        sem_wait(&comida);
+        pthread_mutex_lock(&protege_porcoes);
+        porcoes--;
+        if(porcoes == 0) 
         {
+            // chama_cozinheiro(&id);
+            sem_wait(&cozinhando);
+            sem_wait(&enchendo);
+        }
+        pthread_mutex_unlock(&protege_porcoes);
+        come(&id);
+        
+        // pthread_mutex_lock(&mtx_porcoes);
+        // if(porcoes <= 0) 
+        // {
             // printf("porcoes 1: %d\n", porcoes);
             // pthread_mutex_lock(&mtx_travessa_vazia);
             
-            printf("id: %d\n", id);
-            // printf("porcoes: %d\n", porcoes);
-            printf("acordando o cozinheiro\n");
-            sleep(1);
-            travessa_vazia = 1;
+        //     printf("id: %d\n", id);
+        //     // printf("porcoes: %d\n", porcoes);
+        //     printf("acordando o cozinheiro\n");
+        //     sleep(1);
+        //     travessa_vazia = 1;
 
-            // pthread_mutex_unlock(&mtx_travessa_vazia);
-            pthread_mutex_unlock(&mtx_porcoes);
-        } 
-        else 
-        {
-            printf("o canibal %d está se servindo\n", id);
-            porcoes--;
-            sleep(1); 
-            pthread_mutex_unlock(&mtx_porcoes);
+        //     // pthread_mutex_unlock(&mtx_travessa_vazia);
+        //     pthread_mutex_unlock(&mtx_porcoes);
+        // } 
+        // else 
+        // {
+        //     printf("o canibal %d está se servindo\n", id);
+        //     porcoes--;
+        //     sleep(1); 
+        //     pthread_mutex_unlock(&mtx_porcoes);
             
-            printf("o canibal %d está comendo\n", id);
-            sleep(1);
-        }
+        //     printf("o canibal %d está comendo\n", id);
+        //     sleep(1);
+        // }
     }
 }
 
-void *cozinhando(void *arg)
+void *cozinha(void *arg)
 {
+    // int M = M;
+    int M = *((int *) arg);
     while(1)
     {
         // localização dos mutex está correta?
-        pthread_mutex_lock(&mtx_travessa_vazia);
-        if(travessa_vazia == 1)
-        {
-            printf("enchendo a travessa\n");
-            sleep(1);
+        // pthread_mutex_lock(&mtx_travessa_vazia);
+        // if(travessa_vazia == 1)
+        // {
+        //     printf("enchendo a travessa\n");
+        //     sleep(1);
 
-            porcoes = max_porcoes;
-            travessa_vazia = 0;
-            printf("travessa cheia\n");
+        //     porcoes = max_porcoes;
+        //     travessa_vazia = 0;
+        //     printf("travessa cheia\n");
+        // }
+        // pthread_mutex_unlock(&mtx_travessa_vazia);
+
+        sem_post(&cozinhando);
+        enche_travessa();
+        for(int i = 0; i < M; i++)
+        {
+            sem_post(&comida);
         }
-        pthread_mutex_unlock(&mtx_travessa_vazia);
+        porcoes = M;
+        sem_post(&enchendo);
     }
 }
 
@@ -73,20 +122,18 @@ int main(int argc, char *argv[])
 
     pthread_t canibais[N];
     porcoes = M; 
-    max_porcoes = M;
     pthread_t cozinheiro = 0; 
 
-    // precisa usar mais de um mutex?
-    pthread_mutex_init(&mtx_travessa_vazia, NULL);
-    pthread_mutex_init(&mtx_porcoes, NULL);
+    pthread_mutex_init(&protege_porcoes, NULL);
 
-    pthread_create(&cozinheiro, NULL, cozinhando, NULL);
+    int *novo_m = (int *) malloc(sizeof(int));
+    *novo_m = M; 
+    pthread_create(&cozinheiro, NULL, cozinha, (void *) novo_m);
 
     for(int i = 0; i < N; i++) 
     {
         int *id = (int *) malloc(sizeof(int));
         *id = i + 1;
-        // tem que especificar algo no último argumento? por quê?
         pthread_create(&canibais[i], NULL, canibal, (void *) id);
     }
 
